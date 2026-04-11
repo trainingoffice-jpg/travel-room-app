2032b5d9d1a234968a014958813c44d8f3548a28
trainingoffice-jpg <trainingoffice@grtjewels.com>
2026-04-11 13:10:09 +0530

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  import.meta.env.https://bfkfvelroahfpascfuhj.supabase.co
-,
-  import.meta.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJma2Z2ZWxyb2FoZnBhc2NmdWhqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU4ODQwMjIsImV4cCI6MjA5MTQ2MDAyMn0.nlkVNMMzQUacaD_3sqe7btkuQxxdbNVC4_tfdQuPAGk
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
 );
